1c02d05ef90898f7c673a20bf530c4142afcf73b
Anthony Fouchard <anthony.fouchard.rt@gmail.com>
2025-12-03 12:09:21 +0700

    [ADD] files in gitignore

---
+++ scripts/cycle_windows_dofus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+STATE_FILE="/tmp/dofus_window_index"
+CLASS_INI=('Cra' 'Feca' 'Enu')
+AVAILABLE=($(wmctrl -l | grep "Dofus-" | awk '{print $4}' | cut -d'-' -f2))
+if [[ ${#AVAILABLE[@]} -eq 0 ]]; then exit 1; fi
+if [ -f "$STATE_FILE" ]; then INDEX=$(cat "$STATE_FILE"); else INDEX=0; fi
+TOTAL=${#CLASS_INI[@]}
+for ((i=1; i<=TOTAL; i++)); do
+    NEXT=$(( (INDEX + i) % TOTAL ))
+    CLASS_NAME=${CLASS_INI[$NEXT]}
+    if printf '%s\n' "${AVAILABLE[@]}" | grep -q "^$CLASS_NAME$"; then
+        wmctrl -a "Dofus-$CLASS_NAME"
+        echo "$NEXT" > "$STATE_FILE"
+        exit 0
+    fi
+done
